de434e329770342a1ae2d432d0682bb4642bca1d
Brian Warchesik <warchesikarts@gmail.com>
2019-01-28 11:36:26 -0700

    Checkpoint simple to do part two complete

--- app.js
+++ app.js
@@ -1,39 +1,49 @@
 function onReady() {
     const ADD_TODO_FORM = document.getElementById('addToDoForm');
-    const NEW_TODO_TEXT = document.getElementById('newToDoText');
-    const TODO_LIST = document.getElementById('toDoList');
+    let toDos = [];
+    let id = 0;
+
+    function renderTheUI() {
+        const TODO_LIST = document.getElementById('toDoList');
+
+        TODO_LIST.textContent = "";
+
+        toDos.forEach(function(toDo) {
+            const NEW_LI = document.createElement('li');
+            const CHECKBOX = document.createElement('input');
+            CHECKBOX.type = "checkbox";
+
+            NEW_LI.textContent = toDo.title;
+            NEW_LI.prepend(CHECKBOX);
+
+            TODO_LIST.appendChild(NEW_LI);
+        });
+    }
+
+    function createNewToDo() {
+        const NEW_TODO_TEXT = document.getElementById('newToDoText');
+        if (!NEW_TODO_TEXT.value) {
+            return;
+        }
+        toDos.push({
+            title: NEW_TODO_TEXT.value,
+            complete: false,
+            id: id
+        });
+
+        id++
+
+        console.log(toDos);
+        newToDoText.value = '';
+        renderTheUI();
+    }
 
     ADD_TODO_FORM.addEventListener('submit', event => {
         event.preventDefault();
-        // get the text //
-        let title = NEW_TODO_TEXT.value;
-        // create a new li//
-        let newLi = document.createElement('li');
-        // create new input //
-        let checkbox = document.createElement('input');
-        // create delete button //
-        let deleteBtn = document.createElement('button');
-        deleteBtn.textContent = "—";
-        deleteBtn.classList.add("deleteBtn");
+        createNewToDo();
+    })
 
-        deleteBtn.addEventListener('click', function(event) {
-            // console.log(event); //
-            TODO_LIST.removeChild(this.parentElement);
-        });
-
-        // set input's type to checkbox //
-        checkbox.type = "checkbox";
-        // set the title //
-        newLi.textContent = title;
-        // attach delete button to li //
-        newLi.prepend(deleteBtn);
-        // attach the checkbox to li //
-        newLi.prepend(checkbox);
-        // attach the li to the ul //
-        toDoList.appendChild(newLi);
-        //empty the input field//
-        NEW_TODO_TEXT.value = "";
-    });
+    renderTheUI();
 }
 
 
